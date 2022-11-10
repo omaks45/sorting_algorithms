fcda83fc6d456d3c63be550847682d94bc655333
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-11-10 04:01:22 +0100

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,41 +1,45 @@
 #include "sort.h"
+
 /**
-* swap_ints - swaps two integers in an array
-* @x: the first integer to swap.
-* @y: the second integer to swap.
-*/
+ * swap_ints - Swap two integers in an array.
+ * @x: The first integer to swap.
+ * @y: The second integer to swap.
+ */
 void swap_ints(int *x, int *y)
 {
-	int temp;
+	int tmp;
 
-	temp = *x;
+	tmp = *x;
 	*x = *y;
-	*y = temp;
+	*y = tmp;
 }
 
 /**
-* selection_sort - sorts array of integers in ascending order
-* using the selection algorithm
-* @array: array of integers
-* @size: size of the array
-*
-* Description: print the array after each swap
-*/
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
 void selection_sort(int *array, size_t size)
 {
+	int *min;
 	size_t i, j;
 
 	if (array == NULL || size < 2)
 		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
+		min = array + i;
 		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
 		{
-			if (array[i] > array[j])
-			{
-				swap_ints(&array[i], &array[j]);
-				print_array(array, size);
-			}
+			swap_ints(array + i, min);
+			print_array(array, size);
 		}
 	}
 }
